c78def44ee2645aa6ba97f1ec47c153aba1a7896
Jorge Soares <junior-soares42@hotmail.com>
2018-06-21 09:36:15 -0300

    curso.py

--- curso.py
+++ curso.py
@@ -1,3 +1,9 @@
-n  = input("Digite seu nome")
+n  = input("Digite seu nome: ")
+i  = int(input("Digite sua idade: "))
 
-print('Hello World! {}'.format(n))
+print('Hello World! {}, voce tem {} anos. '.format(n,i))
+
+if i<18:
+	print('Menor de idade')
+else:
+	print('Maior de idade')	
