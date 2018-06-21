f6c4d4348a7d4c420e475dcba3578ef76f33b347
Jorge Soares <junior-soares42@hotmail.com>
2018-06-21 20:48:48 -0300

    adicionado o pais

--- curso.py
+++ curso.py
@@ -1,9 +1,10 @@
 n  = input("Digite seu nome: ")
 i  = int(input("Digite sua idade: "))
-
-print('Hello World! {}, voce tem {} anos. '.format(n,i))
+p = input("Qual o seu pais? ")
+print('Hello World! {}, voce tem {} anos.\nComo vai o tempo em {} '.format(n,i,p))
 
 if i<18:
 	print('Menor de idade')
 else:
 	print('Maior de idade')	
+
